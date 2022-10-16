85ba892fb1ce7befdcf5d4a069eda15b9f9343bd
Antonio Lima <37549423+Negwar@users.noreply.github.com>
2022-10-16 19:09:43 -0300

    commit inicial

---
+++ src/js/index.js
@@ -0,0 +1,52 @@
+//OBJETIVO
+/*Quando clicar na aba temos que mostrar o conteúdo 
+da aba que foi clicada pelo usuário e esconder o 
+conteúdo da aba anterior.
+
+- passo 1: dar um jeito de pegar os elementos que representam 
+as abas no html
+- passo 2: dar um jeito de identificar o clique no elemento 
+da aba
+- passo 3: quando o usuario clicar, desmarcar a aba selecionada
+- passo 4: marcar a aba clicada como selecionado
+- passo 5: esconder o conteudo anterior
+- passo 6: mostrar o conteudo da aba selecionada
+*/
+
+
+//passo 1
+const abas = document.querySelectorAll(".aba");
+
+
+abas.forEach(aba => {
+    //passo 2
+    aba.addEventListener("click", function(){
+
+        if(aba.classList.contains("selecionado")){
+            return;
+        }
+
+        selecionarAba(aba)
+        mostrarInformacoesDaAba(aba)
+    })
+})
+
+function selecionarAba(aba){
+     // //passo 3
+        const abaSelecionada = document.querySelector(".aba.selecionado");
+        abaSelecionada.classList.remove("selecionado");
+
+        // //passo 4
+        aba.classList.add("selecionado");
+}
+
+function mostrarInformacoesDaAba(aba){
+     //passo 5
+     const informacaoSelecionada = document.querySelector(".informacao.selecionado");
+     informacaoSelecionada.classList.remove("selecionado");
+
+     //passo 6
+     const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`;
+     const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba);
+     informacaoASerMostrada.classList.add("selecionado");
+}
